27971c290abada7a452b6f7ecbd19a5e3df3c40f
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-03-06 16:42:25 -0600

    success import

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import AvatarList from './components/AvatarList';
 
 function App() {
-  const contacts = ["Olivia", "Ethan", "Sophia", "William", "Isabella", "Benjamin", "Mia", "Alexander", "Charlotte", "James"];
+  
   return (
     <div className="App">
-      <div className="AvatarLeft">
-        <div className="Avatar">
-        <img src={logo} className="App-logo" alt="logo" />
-        </div>
-        <div>
-        {contacts.map((item,index) => (
-          <div key={index}>
-            <p>{item}</p>
-          </div>
-        ))}
-        </div>
-      </div>
+     <AvatarList/>
     </div>
   );
 }
