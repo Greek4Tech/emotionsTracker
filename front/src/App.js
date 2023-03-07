3e58da78a30076c8d81c32fbe2ce09b5405c142f
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-03-07 14:13:38 -0600

    removed       <div>{selectedDate.toLocaleString()}</div> from App

--- front/src/App.js
+++ front/src/App.js
@@ -13,7 +13,6 @@ function App() {
     <div className="App">
      <AvatarList/>
      <MiniMonthNavigator onChange={handleDateChange} />
-      <div>{selectedDate.toLocaleString()}</div>
     </div>
   );
 }
